4831d72069b32a314603a247086cf6996dd6f93b
StevenDobb <steven.dobbelaere@hotmail.com>
2017-04-19 11:52:15 +0200

    take a break

--- Xploration/src/org/xploration/team4/AgCompany4.java
+++ Xploration/src/org/xploration/team4/AgCompany4.java
@@ -2,6 +2,10 @@ package org.xploration.team4;
 
 import org.xploration.ontology.*;
 
+import jade.content.AgentAction;
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.basic.Action;
 import jade.core.Agent;
 import jade.core.behaviours.*;
 import jade.core.AID;
@@ -15,95 +19,104 @@ import java.util.Date;
 public class AgCompany4 extends Agent {
 	 
 	private static final long serialVersionUID = 1L;
-	public final static String TEAM_ID = "4";
+	public final static int TEAM_ID = 4;
+	// The name of the service the Registration Desk agent is using to announce itself in the Yellow Pages
 	public final static String REGISTRATION_DESK_NAME = "registrationDesk";
 	public final static String REQUEST_SUCCEEDED = "Request: Success";
 	public final static String REQUEST_FAULED = "Request: Failure";
 
+	//sources: 
+	//  http://paginas.fe.up.pt/~eol/SOCRATES/Palzer/ontologysupportJADE.htm
+	//  https://www.iro.umontreal.ca/~vaucher/Agents/Jade/Ontologies.htm
+	private Codec codec = new SLCodec();
+	private jade.content.onto.Ontology ontology = XplorationOntology.getInstance();
 	protected void setup()
 	{
 		System.out.println(getLocalName()+": HAS ENTERED"); //TODO what is LocalName? Better AgCompany4 ?
 		
-		//source: http://paginas.fe.up.pt/~eol/SOCRATES/Palzer/ontologysupportJADE.htm
-		jade.content.onto.Ontology ontology = XplorationOntology.getInstance();
+		getContentManager().registerLanguage(codec);
         getContentManager().registerOntology(ontology);
 		
 		// Add a behavior to register to the registration desk
-		// IF registration is successful, ok 
-		// ELSE, waits 5 seconds and tries again
+		// IF registration is successful, it is saved in the boolean
+		// ELSE an error message is printed
 		addBehaviour(new SimpleBehaviour(this)
 		{
-			private static final long serialVersionUID =1L;
-			boolean end = false;
-			AID[] painters = new AID[20];
+			
+			private static final long serialVersionUID = 1L;
+			private boolean registrationSuccess = false;
+//			private boolean timedOut = false;
 			AID ag;
-			boolean ignore = false;
-			int last = 0;
-			int i, j;
 
 			public void action()
 			{   
-				// Creates the description for the type of agent to be searched
-				DFAgentDescription dfd = new DFAgentDescription();
-				ServiceDescription sd = new ServiceDescription();
-				sd.setType(PAINTER);
-				dfd.addServices(sd);
-
-				try
+				// Only register if not registered yet  
+				if (!registrationSuccess) 
 				{
-					// If has been searching for estimations for more than a minute, it does not search any more
-					if ((new Date()).getTime() - registerTime.getTime() >= 60000)
+					// Creates the description for the type of agent to be searched in the yellow pages
+					DFAgentDescription dfd = new DFAgentDescription();
+					ServiceDescription sd = new ServiceDescription();
+					sd.setType(REGISTRATION_DESK_NAME);
+					dfd.addServices(sd);
+	
+					try
 					{
-						end = true;
-					}
-
-					// It finds agents of the required type
-					DFAgentDescription[] res = new DFAgentDescription[20];
-					res = DFService.search(myAgent, dfd);
-
-					// Gets the first occurrence, if there was success
-					if (res.length > 0)
-					{
-						for (i=0; i < res.length; i++)
-						{
-							ag = (AID)res[i].getName();
-
-							for (j=0; j<last; j++)
+						
+							// It finds agents of the required type
+							DFAgentDescription[] res = new DFAgentDescription[20];
+							res = DFService.search(myAgent, dfd);
+		
+							// Gets the first occurrence (because we assume there to be one)
+							if (res.length > 0)
 							{
-								if (painters[j].compareTo(ag) == 0)
-								{
-									ignore = true;
-								}
+								ag = (AID)res[0].getName();
+		
+								// Try to register to the desk
+								org.xploration.ontology.Team team = new Team();
+								team.setTeamId(TEAM_ID);
+								org.xploration.ontology.RegistrationRequest regReq = new RegistrationRequest();
+								regReq.setTeam(team);
+								AgentAction regReqAction = new Action(ag, regReq);
+								sendMessage(ACLMessage.REQUEST, regReqAction);
+								System.out.println(getLocalName()+": TRY TO REGISTER");
+								
+								//TODO process answers and add printing messages with possible outcomes (too late, already registered) ..
+								ACLMessage ans = receive(MessageTemplate.MatchPerformative(ACLMessage.AGREE));
+								
+//								doWait(5000);
 							}
-							if (!ignore)
+							else
 							{
-								painters[last++] = ag;
-								// Asks the estimation to the painter
-								ACLMessage msg = new ACLMessage(ACLMessage.CFP);
-								msg.setContent(ESTIMATION);
-								msg.addReceiver(ag);
-								send(msg);
-								System.out.println(getLocalName()+": ASKS FOR AN ESTIMATION");
+								// If no registration desk has been found, it waits 5 seconds
+								doWait(5000);
 							}
-							ignore = false;
-						}
-						doWait(5000);
+						
 					}
-					else
+					catch (Exception e) 
 					{
-						// If no PAINTER has been found, it waits 5 seconds
-						doWait(5000);
+						e.printStackTrace();
 					}
 				}
-				catch (Exception e) 
-				{
-					e.printStackTrace();
-				}
+			}
+			
+			void sendMessage(int performative, AgentAction action) {
+				ACLMessage msg = new ACLMessage(performative);
+				
+				msg.setLanguage(codec.getName());
+                msg.setOntology(ontology.getName());
+                try {
+                	getContentManager().fillContent(msg, new Action(server, action));
+                	msg.addReceiver(ag);
+                	send(msg);
+                	}
+                catch (Exception ex) { ex.printStackTrace(); }
+			}
+				send(msg);
 			}
 
 			public boolean done ()
 			{
-				return end;
+				return registrationSuccess;
 			}
 
 		});
--- Xploration/src/org/xploration/team4/platform/RegistrationDesk.java
+++ Xploration/src/org/xploration/team4/platform/RegistrationDesk.java
@@ -2,6 +2,7 @@ package org.xploration.team4.platform;
 
 import jade.core.Agent;
 
+//TODO use DFRegisterAgent at the beginning? 
 public class RegistrationDesk extends Agent {
 	public static final String NAME = "registrationDesk";
 
