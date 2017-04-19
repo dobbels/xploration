bd86c2d9b99638142c7b7a6d1bd83fafb22e0a98
StevenDobb <steven.dobbelaere@hotmail.com>
2017-04-19 13:47:22 +0200

    add empty JUnit classes with commented possible test cases

---
+++ Xploration/src/org/xploration/tests/CompanyRegistration.java
@@ -0,0 +1,5 @@
+package org.xploration.tests;
+
+public class CompanyRegistration {
+
+}
---
+++ Xploration/src/org/xploration/tests/RegistrationDeskTest.java
@@ -0,0 +1,19 @@
+package org.xploration.tests;
+
+/*
+ * Possible test cases: 
+ * 
+ * Start up a certain Registration Desk with registration period X.
+ * 
+ * Case 1: Send a request and get an AGREE and an INFORM
+ * 
+ * Case 2: Send another request (within registration period) and get AGREE and FAILURE
+ * 
+ * Case 3: Send rubbish and get NOT_UNDERSTOOD
+ * 
+ * Case 4: Send another request outside of registration period and get REFUSE
+ * 
+ */
+public class RegistrationDeskTest {
+
+}
