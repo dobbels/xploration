e69df10add262251cf8c485d844b76c1968dcd7c
Gokhan Aker <gokhanaker@gmail.com>
2017-05-02 22:35:27 +0200

    trying

--- Xploration/src/org/xploration/team4/company/Constants.java
+++ Xploration/src/org/xploration/team4/company/Constants.java
@@ -1,8 +1,20 @@
 package org.xploration.team4.company;
+import java.util.*;
 
 public class Constants {
 	// The name of the service the Registration Desk agent is using to announce itself in the Yellow Pages
 	public final static String REGISTRATION_DESK_NAME = "registrationDesk";
 	
 	public final static int TEAM_ID = 4;
+	
+	//Map 5X5 sizes with A,B,C,D minerals
+	/*
+	String [][] multi = new String [5][5];	
+	multi[1][1] = "A"; multi[1][3] = "B"; multi[1][5] = "C";
+	multi[2][2] = "B"; multi[2][4] = "C"; 
+	multi[3][1] = "D"; multi[3][3] = "B"; multi[3][5] = "D";
+	multi[4][2] = "C"; multi[4][4] = "A";
+	multi[5][1] = "D"; multi[5][3] = "C"; multi[5][5] = "A";	
+	*/
+	
 }
