7ce7bab559d13477d1e18649aca389543ecc65d2
Unknown <steven.dobbelaere@student.kuleuven.be>
2017-05-23 23:48:00 +0200

    Change MapReader to read the correct format

--- Xploration/src/org/xploration/team4/platform/MapReader.java
+++ Xploration/src/org/xploration/team4/platform/MapReader.java
@@ -3,6 +3,7 @@ package org.xploration.team4.platform;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -11,7 +12,7 @@ import org.xploration.team4.common.Map;
 
 public class MapReader {
 	
-	private final static String MAP_FILE = "./src/org/xploration/team4/platform/MAP_FILE.txt";
+	private final static String MAP_FILE = "./src/org/xploration/team4/platform/map.txt";
 	
 	//worldMap construction from a read txt file
 	public static Map readMap() {
@@ -29,28 +30,42 @@ public class MapReader {
 			//text has the lines
 			int x = 1;
 			
+			
+			
 			while ((text = reader.readLine()) != null)
 			{	
-				if (!dimensionsRead) {
-					String[] dims = text.split(",");
-					dims[0] = dims[0].substring(1);
-					dims[1] = dims[1].substring(0, dims[1].length()-1);
-					dimX = Integer.parseInt(dims[0]);
-					dimY = Integer.parseInt(dims[1]);
-					map = new Map(dimX, dimY);
-					dimensionsRead = true;
-				} 
-				else {
-					if (!(text.length() == dimY))
-						System.out.println("Inconsistent map dimensions !!");
-					for(int y = 1; y <=text.length(); y++){
-						if (!(map.getCell(x, y) == null)) {
-							if (text.substring(y-1, y).equals(" "))
-								System.out.println("Parsing error");
-							map.putMineral(x, y, text.substring(y-1, y));
+				if (!text.equals("")) { // ignore white lines
+					if (!dimensionsRead) {
+						String[] dims = text.split(",");
+						dims[0] = dims[0].substring(1);
+						dims[1] = dims[1].substring(0, dims[1].length()-1);
+						dimX = Integer.parseInt(dims[0]);
+						dimY = Integer.parseInt(dims[1]);
+						map = new Map(dimX, dimY);
+						dimensionsRead = true;
+					} 
+					else {
+						if (x%2 == 1) {
+							for(int y = 1; y <=text.length(); y++){
+								if (!(map.getCell(x, y) == null)) {
+									if (text.substring(y-1, y).equals(" "))
+										System.out.println("Parsing error");
+									map.putMineral(x, y, text.substring(y-1, y));
+								}
+							}
 						}
+						else {
+							for(int y = 1; y <=text.length(); y++){
+								if (!(map.getCell(x, y+1) == null)) {
+									if (text.substring(y-1, y).equals(" "))
+										System.out.println("Parsing error");
+									map.putMineral(x, y+1, text.substring(y-1, y));
+								}
+							}
+						}
+						
+						x++;
 					}
-					x++;
 				}
 			}
 		} catch (IOException e) {
@@ -65,6 +80,15 @@ public class MapReader {
 			} catch (IOException e) {}
 		}
 		return map;
+	}
+	
+	//To test the parsing
+	public static void main(String[] args) {
+		
+		Map m = readMap();
+		
+		m.printWorldMap();
+		
 	}
 
 }
